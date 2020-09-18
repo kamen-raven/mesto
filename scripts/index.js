f872d2c66f54f0fc15834a30e600a488f4eb8231
Slava Kamenev <slava-stavropol@yandex.ru>
2020-09-19 00:59:42 +0300

    feat: создание исходных карточек

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,42 @@ const initialCards = [
   }
 ];
 
+const cardsContainer = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.template-cards').content;
+
 
 const profileTxtName = document.querySelector('.profile__title');
 const profileTxtAbout = document.querySelector('.profile__subtitle');
+
 const popupEdit = document.querySelector('.popup');
 const popupEditButtonOpen = document.querySelector('.profile__edit-button');
 const popupEditButtonClose = popupEdit.querySelector('.popup__close-button');
 const popupForm = popupEdit.querySelector('.popup__profile-form');
 const popupTxtName = popupEdit.querySelector('.popup__input_txt_name');
 const popupTxtAbout = popupEdit.querySelector('.popup__input_txt_about');
+
+
+
+
+//создание "изначальных" карточек
+const originalCards = ({name, link}) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = name;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardsContainer.appendChild(cardElement);
+}
+
+
+const createCards = () => {
+  cardsContainer.innerHTML = "";
+  initialCards.forEach(originalCards);
+}
+
+createCards();
+
+
+
 
 //загрузка текста в попап редактирования профиля
 const loadProfileInfo = () => {
@@ -68,3 +95,4 @@ const popupSaveForm = (event) => {
 popupEditButtonOpen.addEventListener('click', popupOpen);
 popupEditButtonClose.addEventListener('click', popupClose);
 popupForm.addEventListener('submit', popupSaveForm);
+
