800b04b9f61f1dbdfa66611b1a76fc5265db63e5
Slava Kamenev <slava-stavropol@yandex.ru>
2020-09-18 20:49:11 +0300

    fix: grid-card

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
 const profileTxtName = document.querySelector('.profile__title');
 const profileTxtAbout = document.querySelector('.profile__subtitle');
 const popupEdit = document.querySelector('.popup');
@@ -7,27 +35,31 @@ const popupForm = popupEdit.querySelector('.popup__profile-form');
 const popupTxtName = popupEdit.querySelector('.popup__input_txt_name');
 const popupTxtAbout = popupEdit.querySelector('.popup__input_txt_about');
 
-
-const loadProfileInfo = function () {
+//загрузка текста в попап редактирования профиля
+const loadProfileInfo = () => {
   popupTxtName.value = profileTxtName.textContent.trim();
   popupTxtAbout.value = profileTxtAbout.textContent.trim();
 }
 
-const saveProfileInfo = function () {
+//передача текста из попапа редактирования профиля
+const saveProfileInfo = () => {
   profileTxtName.textContent = popupTxtName.value;
   profileTxtAbout.textContent = popupTxtAbout.value;
 }
 
-const popupOpen = function () {
+//открытие попапа редактирования профиля профиля
+const popupOpen = () => {
   loadProfileInfo();
   popupEdit.classList.add('popup_opened');
 };
 
-const popupClose = function () {
+//закрытие попапа редактирования профиля
+const popupClose = () => {
   popupEdit.classList.remove('popup_opened');
 };
 
-const popupSaveForm = function () {
+//сохранение попапа редактирования профиля
+const popupSaveForm = (event) => {
   event.preventDefault();
   saveProfileInfo();
   popupClose();
